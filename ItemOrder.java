c3eeb405b5310894a41848476fe844514baacae7
vpritamani <44382864+vpritamani@users.noreply.github.com>
2020-12-06 22:06:55 -0800

    Documentation complete on ItemOrder.java

--- ItemOrder.java
+++ ItemOrder.java
@@ -1,9 +1,13 @@
 public class ItemOrder {
 
+    // variables for each object of an ItemOrder
     private double price;
     private Item item;
     public int quantity = 0;
 
+    // constructor - sets quantity to input if inputtedQuantity > 0, else
+    // it is set to 0. Also calls Item's priceFor method to set price
+    // variable to the desired amount based off quantity
     public ItemOrder(Item item, int quantity){
         this.item = item;
         if (!(quantity > 0)){
@@ -12,6 +16,7 @@ public class ItemOrder {
         this.price = item.priceFor(quantity);
     }
 
+    // get methods
     public double getPrice(){
         return this.price;
     }
